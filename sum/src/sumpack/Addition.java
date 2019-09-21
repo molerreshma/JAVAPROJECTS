6d152d96539b74fd7f0d1c9a4b5b4a003265852c
Face <Face@Face-PC>
2019-09-21 00:22:37 -0800

    sum

---
+++ sum/src/sumpack/Addition.java
@@ -0,0 +1,17 @@
+package sumpack;
+import java.util.Scanner;
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+Scanner Sc=new Scanner(System.in);
+int a = Sc.nextInt();
+int b = Sc.nextInt();
+int sum = a+b;
+System.out.println(sum);
+
+	}
+
+}
